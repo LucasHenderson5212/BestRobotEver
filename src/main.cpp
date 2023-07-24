24808d0027fdbc4c6f31db7892ad84e184c93212
Levivus <levi.varsanyi@gmail.com>
2023-07-24 15:16:31 -0700

    New PID system, with framework of states

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,15 @@
 #include <Adafruit_SSD1306.h>
 #include <math.h>
 
+#define displayOn false
+
+#define TAPE_FOLLOW_STATE 1
+#define COLLISION_STATE 2
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET  -1 // This display does not have a reset pin accessible
+
 #define RIGHT_EYE PA3
 #define LEFT_EYE PA2
 #define RIGHT_EYE_OUT PA4
@@ -16,7 +22,7 @@
 #define RIGHT_MOTOR PB_9
 #define RIGHT_MOTOR_2 PB_8
 
-#define BOX_MOTOR PA8
+#define BOX_MOTOR PA_9
 #define MAG_SENSOR PB4
 #define MAG_SENSOR_2 PB5
 #define MAG_SENSOR_3 PB3
@@ -28,20 +34,22 @@
 #define LEFT_DOOR PA_0
 #define RIGHT_DOOR PA_0
 
-#define LEFT_DOOR_CLOSE 0
-#define LEFT_DOOR_OPEN 0
-#define RIGHT_DOOR_CLOSE 0
-#define RIGHT_DOOR_OPEN 0
+#define LEFT_DOOR_CLOSE 500
+#define LEFT_DOOR_OPEN 200
+#define RIGHT_DOOR_CLOSE 950
+#define RIGHT_DOOR_OPEN 1250
 
-#define STEERING_NEUTRAL 1380
-#define STEERING_MIN_INPUT 1000
-#define STEERING_MAX_INPUT 1800
+#define STEERING_NEUTRAL 1500
+#define STEERING_MIN_INPUT 1040
+#define STEERING_MAX_INPUT 1960
 
-#define PID_MIN STEERING_MIN_INPUT//-1000//-1*MOTOR_SPEED
-#define PID_MAX STEERING_MAX_INPUT//+1000//+1*MOTOR_SPEED
+#define MAX_LEFTOVER (4095+MOTOR_SPEED)/kdif
+
+#define PID_MIN STEERING_MIN_INPUT-MAX_LEFTOVER
+#define PID_MAX STEERING_MAX_INPUT+MAX_LEFTOVER
 
 #define MOTOR_FREQUENCY 100
-#define MOTOR_SPEED 1000
+#define MOTOR_SPEED 1100
 
 #define THRESHOLD 180
 
@@ -50,19 +58,24 @@
 #define STATE_3 4
 #define STATE_4 7
 
-#define displayOn true
-
 //PID constants
-#define kp 11
-#define kd 145
-#define kdout 65
-#define ki 1 //start in the order of ~1 in testing
-#define kdif 3.5 //0.05
+#define kp 110
+#define kd 250
+#define ki 1
+#define kdif 4.5
 
-#define maxi 36
+#define kWheelSpeedUp (4000-MOTOR_SPEED)/(MAX_LEFTOVER*kdif)*0.1
+//leftOver*kdif*0.x < 4000 - MOTOR_SPEED
+
+#define dDuration 12
+
+#define maxi 25
 
 int steeringState = 0;
 int i = 0;
+
+int dCount = 0;
+int lastD = 0;
 
 int currentServoPos = STEERING_NEUTRAL;
 int currentPIDNum = STEERING_NEUTRAL;
@@ -71,7 +84,10 @@ int lastTime;
 
 void magnet_interrupt(void);
 void magnet_out_interrupt(void);
-void boxes();
+
+
+void follow_tape(void);
+int get_state(void);
 
 Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
@@ -87,7 +103,6 @@ void setup() {
   }
 
   //Set Servo to neutral position
-  currentServoPos = STEERING_NEUTRAL;
   pwm_start(STEERING_SERVO, 50, currentServoPos, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
 
   // //Tape Following
@@ -101,18 +116,23 @@ void setup() {
   pinMode(MAG_SENSOR_2, INPUT);
   pinMode(MAG_SENSOR_3, INPUT);
   pinMode(MAG_SENSOR_4, INPUT);
-  // pinMode(MAG_OUT_SENSOR, INPUT);
-  // pinMode(MAG_OUT_SENSOR_2, INPUT);
+  pinMode(MAG_OUT_SENSOR, INPUT);
+  pinMode(MAG_OUT_SENSOR_2, INPUT);
 
   attachInterrupt(digitalPinToInterrupt(MAG_SENSOR), magnet_interrupt, FALLING);
   attachInterrupt(digitalPinToInterrupt(MAG_SENSOR_2), magnet_interrupt, FALLING);
   attachInterrupt(digitalPinToInterrupt(MAG_SENSOR_3), magnet_interrupt, FALLING);
   attachInterrupt(digitalPinToInterrupt(MAG_SENSOR_4), magnet_interrupt, FALLING);
-  // attachInterrupt(MAG_OUT_SENSOR, magnet_out_interrupt, FALLING);
-  // attachInterrupt(MAG_OUT_SENSOR_2, magnet_out_interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(MAG_OUT_SENSOR), magnet_out_interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(MAG_OUT_SENSOR_2), magnet_out_interrupt, FALLING);
 
   //Start Box Motor
-  digitalWrite(BOX_MOTOR, HIGH);
+  pwm_start(BOX_MOTOR, 100, 4095, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
+
+  //Opens doors
+  pwm_start(RIGHT_DOOR, 50, RIGHT_DOOR_OPEN, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+  pwm_start(LEFT_DOOR, 50, LEFT_DOOR_OPEN, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+
 
   //for testing
   if(displayOn){
@@ -126,52 +146,36 @@ void setup() {
     display_handler.display();
   }
 
-  digitalWrite(PA8, HIGH);
-
-  delay(10000);
+  delay(100);
 
   lastTime = getCurrentMillis();
 }
 
-void magnet_interrupt(){
-  // digitalWrite(BOX_MOTOR, LOW);
-
-  display_handler.clearDisplay();
-    display_handler.setTextSize(1);
-    display_handler.setTextColor(SSD1306_WHITE);
-    display_handler.setCursor(0,0);
-    display_handler.println("THIS HAPPENED!");
-    display_handler.display();
-
-    // delay(100000);
-
-  //doors close
-  /*
-  pwm_start(RIGHT_DOOR, 50, RIGHT_DOOR_CLOSE, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
-  pwm_start(LEFT_DOOR, 50, LEFT_DOOR_CLOSE, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
-  */
-}
-
-void magnet_out_interrupt(){
-  //digitalWrite(BOX_MOTOR, HIGH);
-  //doors open
-  /*
-  pwm_start(RIGHT_DOOR, 50, RIGHT_DOOR_OPEN, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
-  pwm_start(LEFT_DOOR, 50, LEFT_DOOR_OPEN, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
-  */
-}
-
 
 void loop() {
-  digitalWrite(PA8, LOW);
+  //50 loops per sec
   if((getCurrentMillis() - lastTime) > 20){
-    
     lastTime = getCurrentMillis();
 
-    int r = analogRead(RIGHT_EYE);
-    int ro = analogRead(RIGHT_EYE_OUT);
-    int l = analogRead(LEFT_EYE);
-    int lo = analogRead(LEFT_EYE_OUT);
+    switch (get_state())
+    {
+    case TAPE_FOLLOW_STATE:
+      follow_tape();
+      break;
+    case COLLISION_STATE:
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+void follow_tape(){
+    //Only for display stuff
+    // int r = analogRead(RIGHT_EYE);
+    // int ro = analogRead(RIGHT_EYE_OUT);
+    // int l = analogRead(LEFT_EYE);
+    // int lo = analogRead(LEFT_EYE_OUT);
 
     bool rightOn = analogRead(RIGHT_EYE) > THRESHOLD;
     bool rightOutOn = analogRead(RIGHT_EYE_OUT) > THRESHOLD;
@@ -242,26 +246,36 @@ void loop() {
   
     // calculate PID change
     int p = (int)(kp*steeringState);
-    int d;
-    i = (int)(ki*steeringState) + i;
-    if(abs(lastState) > abs(steeringState)){
-      d = (int)(kd*(steeringState-lastState));
-    } else {
-      //immediately reduce i to 0
-      i = 0;
-      d = (int)(kdout*(steeringState-lastState));
+    int d = (int)(kd*(steeringState-lastState));
+    //d decays after 12 loops
+    if (d == 0 && dCount > 0){
+      d = lastD;
+      dCount--;
+    } else if (d == 0){
+      d = lastD/2;
+    } else if (d != 0) {
+      dCount = dDuration;
+      d += lastD/2;
     }
+    lastD = d;
+    i = (int)(ki*steeringState) + i;
 
+    //Keep i within max
     if (i > maxi){
       i = maxi;
     } else if (i < -maxi){
       i = -maxi;
     }
 
+    // if(abs(lastState) < abs(steeringState)){
+    //   //immediately reduce i to 0
+    //   i = 0;
+    // }
+
     int g = p + d + i;
     
     //Keep numbers within min/max
-    currentPIDNum = currentPIDNum + g;
+    currentPIDNum = STEERING_NEUTRAL + g;
     if(currentPIDNum > PID_MAX) {
       currentPIDNum = PID_MAX;
     } else if (currentPIDNum < PID_MIN){
@@ -283,15 +297,14 @@ void loop() {
     //Set new servo position
     pwm_start(STEERING_SERVO, 50, currentServoPos, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
 
-
     //keep differential within maximum 
-    if (kdif*leftOver > 4000) {
-      leftOver = 4000/kdif;
-    } else if (kdif*leftOver < -4000) {
-      leftOver = -4000/kdif;
+    if (kdif*leftOver-MOTOR_SPEED > 4095) {
+      leftOver = (4095+MOTOR_SPEED)/kdif;
+    } else if (kdif*leftOver+MOTOR_SPEED < -4095) {
+      leftOver = -(4095+MOTOR_SPEED)/kdif;
     }
-
-//
+    
+    //Set Motors
     if(getCurrentMillis() > 2000 && !displayOn){
       if(leftOver > 0){
         if(leftOver*kdif > MOTOR_SPEED){
@@ -301,7 +314,7 @@ void loop() {
           pwm_start(LEFT_MOTOR_2, MOTOR_FREQUENCY, 0, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
           pwm_start(LEFT_MOTOR, MOTOR_FREQUENCY, (int)(MOTOR_SPEED-leftOver*kdif), TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
         }
-        pwm_start(RIGHT_MOTOR, MOTOR_FREQUENCY, MOTOR_SPEED+leftOver*kdif*0.3, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
+        pwm_start(RIGHT_MOTOR, MOTOR_FREQUENCY, MOTOR_SPEED+leftOver*kdif*kWheelSpeedUp, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
       } else if (leftOver < 0){
         if(-leftOver*kdif > MOTOR_SPEED){
           pwm_start(RIGHT_MOTOR, MOTOR_FREQUENCY, 0, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
@@ -310,7 +323,7 @@ void loop() {
           pwm_start(RIGHT_MOTOR_2, MOTOR_FREQUENCY, 0, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
           pwm_start(RIGHT_MOTOR, MOTOR_FREQUENCY, (int)(MOTOR_SPEED+leftOver*kdif), TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
         }
-        pwm_start(LEFT_MOTOR, MOTOR_FREQUENCY, MOTOR_SPEED-leftOver*kdif*0.3, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
+        pwm_start(LEFT_MOTOR, MOTOR_FREQUENCY, MOTOR_SPEED-leftOver*kdif*kWheelSpeedUp, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
       } else {
         pwm_start(RIGHT_MOTOR_2, MOTOR_FREQUENCY, 0, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
         pwm_start(LEFT_MOTOR_2, MOTOR_FREQUENCY, 0, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
@@ -318,10 +331,29 @@ void loop() {
         pwm_start(LEFT_MOTOR, MOTOR_FREQUENCY, MOTOR_SPEED, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
       }
     }
-    //Magnet Stuff?
-  }
-
-  
 }
+
+int get_state(){
+  //This function will use data from the collision sensors to figure out if something other than tape following needs to happen
+  //Most of the time will return TAPE_FOLLOW_STATE
+  return TAPE_FOLLOW_STATE;
+}
+
+void magnet_interrupt(){
+  //Stop motor
+  pwm_start(BOX_MOTOR, 0, 4095, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
+  //doors close
+  pwm_start(RIGHT_DOOR, 50, RIGHT_DOOR_CLOSE, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+  pwm_start(LEFT_DOOR, 50, LEFT_DOOR_CLOSE, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+}
+
+void magnet_out_interrupt(){
+  //start motor
+  pwm_start(BOX_MOTOR, 100, 4095, TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
+  //doors open
+  pwm_start(RIGHT_DOOR, 50, RIGHT_DOOR_OPEN, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+  pwm_start(LEFT_DOOR, 50, LEFT_DOOR_OPEN, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+}
+
 
 
